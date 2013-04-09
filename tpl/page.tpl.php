7b3fd7504c5f79ceacef980c75460a870f1a38ff
Dustin Younse <milsyobtaf@gmail.com>
2013-04-09 17:06:36 -0500

    modified header and navigation output

--- tpl/page.tpl.php
+++ tpl/page.tpl.php
@@ -66,36 +66,36 @@
  * @see template_process()
  */
 ?>
-
   <div id="page" role="document">
     <header id="header" role="banner" class="clearfix">
 
+      <?php print render($page['header_prefix']); ?>
+
+      <a href="http://www.utexas.edu" title="The University of Texas">
+        <img src="<?php print render($theme_path); ?>/images/tower-header.jpg" alt="The University of Texas" /></a>
+
+    <hgroup id="name-and-slogan">
+        <h1 id="site-name">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+        </h1>
       <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
+        <a href="<?php print $front_page; ?>" title="<?php ($site_name)? print $site_name . t('&nbsp;Home') : print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
       <?php endif; ?>
 
-      <?php if ($site_name || $site_slogan): ?>
-        <hgroup id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </h1>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <h3 id="site-slogan"><?php print $site_slogan; ?></h3>
-          <?php endif; ?>
-        </hgroup><!-- #name-and-slogan -->
+      <?php if ($site_slogan): ?>
+        <h3 id="site-slogan"><?php print $site_slogan; ?></h3>
       <?php endif; ?>
+        <img src="<?php print render($theme_path); ?>/images/slogan.png" />
+    </hgroup><!-- #name-and-slogan -->
 
       <?php print render($page['header']); ?>
+
+      <?php print render($page['header_suffix']); ?>
 
     </header>
 
     <!-- navigation -->
-    Hello!
     <?php if ($page['navigation']) :?>
       <?php print render($page['navigation']); ?>
     <?php endif; ?>
